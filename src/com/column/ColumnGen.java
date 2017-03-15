acc4092c04c628a49363db1977f444e0e233188e
Liang-Yuanming <w87754@gmail.com>
2017-03-15 19:45:49 +0800

    Fist Commit

---
+++ src/com/column/ColumnGen.java
@@ -0,0 +1,13 @@
+package com.column;
+
+import ilog.concert.IloObjective;
+import ilog.cplex.IloCplex;
+
+public class ColumnGen {
+	public class MasterProblem{
+		public IloCplex cplex;
+		private IloObjective total_cost;
+
+		
+	}
+}
---
+++ src/com/crop/Crop.java
@@ -0,0 +1,5 @@
+package com.crop;
+
+public class Crop {
+	
+}
