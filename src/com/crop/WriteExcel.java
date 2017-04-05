58425c4515f0f365ef692b0103817bced48ad79e
w87754 <w87754@OptiPlex-9020>
2017-04-05 15:13:15 +0800

    UPDATE WriteExcel.java

--- src/com/crop/WriteExcel.java
+++ src/com/crop/WriteExcel.java
@@ -1,5 +1,12 @@
 package com.crop;
 
-public class WriteExcel {
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
+public class WriteExcel {
+	public void write(){
+		HSSFWorkbook workbook=new HSSFWorkbook();
+		HSSFSheet sheet=workbook.createSheet("Sheet1");
+		
+	}
 }
